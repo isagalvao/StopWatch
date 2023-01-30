7f2c334dbcb4d07dea5a7a2cc35836193948dcc4
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-30 13:29:40 -0300

    starting stopwatch

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StopWatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Start();
+    }
+
+    static void Start()
+    {
+      int time = 10;
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+      }
+    }
+  }
+}
