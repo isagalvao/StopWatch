82c0f5aeae509c778800ea9638871bc0944eb919
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-30 15:12:49 -0300

    uploading project

--- Program.cs
+++ Program.cs
@@ -7,18 +7,46 @@ namespace StopWatch
   {
     static void Main(string[] args)
     {
-      Start(6);
+      Menu();
     }
     static void Menu()
     {
       Console.Clear();
-      Console.WriteLine("Cronometro, Selecione uma opção");
-      Console.WriteLine("S = Segundo");
-      Console.WriteLine("M = Minuto");
-      Console.WriteLine("Finalizar cronometro");
+      Console.WriteLine("Cronometro iniciado, Selecione uma das opção:");
+      Console.WriteLine("");
+      Console.WriteLine("S = Segundo => Exemplo = 10s");
+      Console.WriteLine("M = Minuto => Exemplo = 10m");
+      Console.WriteLine("0 = Sair");
       Console.WriteLine("Quanto tempo deseja contar?");
+
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+
+      int multiplier = 1;
+
+      if (type == 'm')
+        multiplier = 60;
+
+      if (time == 0)
+        System.Environment.Exit(0);
+
+      PreStart(time * multiplier);
+
     }
 
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      Console.WriteLine("Ready...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Go...");
+      Thread.Sleep(2500);
+
+      Start(time);
+    }
     static void Start(int time)
     {
 
@@ -35,6 +63,7 @@ namespace StopWatch
       Console.Clear();
       Console.WriteLine("StopWhatch finalizado");
       Thread.Sleep(2500);
+      Menu();
     }
   }
 }
